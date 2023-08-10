f6d64b5cc8fdff65cfcea4b347f90445484851df
avijit <avijitroy2944@gmail.com>
2023-08-10 09:53:46 +0530

    add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: `http://localhost:3000`,
+    origin: `${process.env.FRONTEND}`,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
